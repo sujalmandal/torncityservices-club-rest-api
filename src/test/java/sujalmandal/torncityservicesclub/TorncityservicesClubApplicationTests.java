3a395b56299fc7db17075dbf1974f000c7101448
Sujal <ss6sujal@gmail.com>
2021-03-27 10:41:52 +0530

    add spring profiles

--- src/test/java/sujalmandal/torncityservicesclub/TorncityservicesClubApplicationTests.java
+++ src/test/java/sujalmandal/torncityservicesclub/TorncityservicesClubApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.util.Assert;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import sujalmandal.torncityservicesclub.services.TornAPIService;
 import sujalmandal.torncityservicesclub.torn.models.PlayerEventsDTO;
 
 @SpringBootTest
+@Profile("test")
 @Slf4j
 class TorncityservicesClubApplicationTests {
 
